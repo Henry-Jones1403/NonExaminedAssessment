df901f5b96e243ca0343ecff7ae121146a1be854
hj210472 <hj210472@reigate.ac.uk>
2022-10-06 10:58:43 +0100

    sql worked on, began process of using objects and started canging labels when button clicked.

--- src/main/java/com/example/nonexaminedassessment/HelloController.java
+++ src/main/java/com/example/nonexaminedassessment/HelloController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import com.example.nonexaminedassessment.UserInfo;
 
 import java.util.ArrayList;
 
@@ -13,9 +14,23 @@ public class HelloController {
     private Label username_label;
 
 @FXML
+private Label FirstName;
+
+@FXML
     public void Search_Button(ActionEvent event){
-    ArrayList <String> sql = SQLhandling.search("SELECT * FROM Accounts", )
-    username_label.setVisible(true);
-    username_label.setText("Hello Mohammed");
+    String name = "Jones";
+    ArrayList <String> sql = SQLhandling.print("SELECT * FROM Users WHERE LastName = 'Jones'", "", "");
+    try{
+        UserInfo Currentuser = new UserInfo(Integer.parseInt(sql.get(0)) , sql.get(1), sql.get(2), sql.get(3), sql.get(4), sql.get(5), Integer.parseInt(sql.get(6)),  sql.get(7));
+        username_label.setVisible(true);
+        FirstName.setVisible(true);
+        username_label.setText(Currentuser.getLastName());
+        FirstName.setText(Currentuser.getFirstName());
+    }catch(Exception e){
+        System.out.println(e);
+    }
+
+
 }
 }
+//WHERE LastName = '" + name + "'
